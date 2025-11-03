4319f1096029b443fa531c18dd663ce0a123dd96
Mike Roberts <mike@sdcs.io>
2025-11-03 08:59:32 -0800

    updates domain name for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
-  site: 'https://lets-carraoke.example',
+  site: 'https://lc-demo.sdcsapp.com',
   output: 'static'
 });
